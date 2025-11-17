08c22cd5d7309ce059bda9e6c9eff0a0d8a7dce4
shahedul-alam <shaheedalamcontact@gmail.com>
2025-11-18 00:58:28 +0600

    Global error handler extended to handle duplicate, cast, zod, validation error

--- src/app/errorHelpers/globalErrorHandler.ts
+++ src/app/errorHelpers/globalErrorHandler.ts
@@ -1,23 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextFunction, Request, Response } from "express";
+import { envVars } from "../config/env";
 import AppError from "../errorHelpers/appError";
-import envVars from "../config/env";
-import { TErrorSources } from "../interfaces/error.interface";
 import { handleDuplicateError } from "../errorHelpers/handleDuplicateError";
 import { handleCastError } from "../errorHelpers/handleCastError";
 import { handleValidationError } from "../errorHelpers/handleValidationError";
 import { handleZodError } from "../errorHelpers/handleZodError";
+import { TErrorSources } from "../interfaces/error.types";
+import { deleteImageFromCloudinary } from "../config/cloudinary.config";
 
-const globalErrorHandler = async (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const globalErrorHandler = async (
   err: any,
   req: Request,
   res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   next: NextFunction
 ) => {
   let statusCode = 500;
   let message = "Internal Server Error";
   let errorSources: TErrorSources[] = [];
+
+  if (req.file) {
+    await deleteImageFromCloudinary(req.file.path);
+  }
+  if (req.files && req.files.length && Array.isArray(req.files)) {
+    const imageUrls = (req.files as Express.Multer.File[]).map(
+      (file) => file.path
+    );
+
+    await Promise.all(imageUrls.map((url) => deleteImageFromCloudinary(url)));
+  }
 
   if (err.code === 11000) {
     const simplifiedError = handleDuplicateError(err);
@@ -52,5 +65,3 @@ const globalErrorHandler = async (
     stack: envVars.NODE_ENV === "development" ? err.stack : null,
   });
 };
-
-export default globalErrorHandler;
---
+++ src/app/errorHelpers/handleZodError.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { TErrorSources, TGenericErrorResponse } from "../interfaces/error.interface";
+
+export const handleZodError = (err: any): TGenericErrorResponse => {
+  const errorSources: TErrorSources[] = [];
+
+  err.issues.forEach((issue: any) =>
+    errorSources.push({
+      path: issue.path[issue.path.length - 1],
+      message: issue.message,
+    })
+  );
+  return {
+    statusCode: 400,
+    message: "Zod error occurred.",
+    errorSources,
+  };
+};
